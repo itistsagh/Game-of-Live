2745a761f5c9a7905993dfc700c8fe0cc5c666a6
itistsagh <tsaghik.h.g@tumo.org>
2020-01-18 17:02:27 +0400

    18.01.2020

---
+++ GrassEater.js
@@ -0,0 +1,71 @@
+class GrassEater extends LivingCreature{
+  
+    NewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+    move() {
+        var emptyCells = this.chooseCell(0);
+        var newCell = random(emptyCells);
+        if (newCell) {
+            var newX = newCell[0];
+            var newY = newCell[1];
+            // console.log(this.x, this.y);
+            matrix[this.y][this.x] = 0;
+            matrix[newY][newX] = this.index;
+            this.x = newX;
+            this.y = newY;
+            this.energy--;
+        }
+    }
+
+    eat() {
+        var emptyCells = this.chooseCell(1);
+        var grass = random(emptyCells);
+        if (grass) {
+            var newX = grass[0];
+            var newY = grass[1];
+            matrix[newY][newX] = this.index;
+            matrix[this.y][this.x] = 0;
+            for (var i in grassArr) {
+                if (newX == grassArr[i].x && newY == grassArr[i].y) {
+                    grassArr.splice(i, 1);
+                    break;
+                }
+            }
+            this.x = newX;
+            this.y = newY;
+            this.energy += 2;
+        }
+    }
+
+    mul() {
+        var newCell = random(this.chooseCell(0))
+        if (newCell && this.energy >= 8) {
+            var newGrassEater = new GrassEater(newCell[0], newCell[1], this.index);
+            GrassEaterArr.push(newGrassEater);
+            matrix[newCell[1]][newCell[0]] = this.index;
+            this.energy = 1;
+
+        }
+    }
+    die() {
+        if (this.energy <= 0) {
+            matrix[this.y][this.x] = 0;
+            for (var i in GrassEaterArr) {
+                if (this.x == GrassEaterArr[i].x && this.y == GrassEaterArr[i].y) {
+                    GrassEaterArr.splice(i, 1)
+                }
+            }
+        }
+    }
+}
